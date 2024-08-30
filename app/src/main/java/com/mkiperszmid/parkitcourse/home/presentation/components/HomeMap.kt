8932637962ae9febcdcf2c2a2ab544dd2e19a31d
Martin Kiperszmid <>
2024-08-29 22:37:35 -0300

    Start home button and google maps animation

--- app/src/main/java/com/mkiperszmid/parkitcourse/home/presentation/components/HomeMap.kt
+++ app/src/main/java/com/mkiperszmid/parkitcourse/home/presentation/components/HomeMap.kt
@@ -1,12 +1,17 @@
 package com.mkiperszmid.parkitcourse.home.presentation.components
 
 
-import android.location.Location
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.mkiperszmid.parkitcourse.home.domain.model.Location
 
 @Composable
 fun HomeMap(
@@ -14,11 +19,33 @@ fun HomeMap(
     carLocation: Location?,
     modifier: Modifier = Modifier
 ) {
+    val cameraPositionState = rememberCameraPositionState()
+
+    LaunchedEffect(currentLocation) {
+        currentLocation?.let {
+            cameraPositionState.animate(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(
+                        it.latitude,
+                        it.longitude
+                    ), 16f
+                )
+            )
+        }
+    }
+
     GoogleMap(
         modifier = modifier,
         properties = MapProperties(
-            isMyLocationEnabled = true
-        )
+            isMyLocationEnabled = true,
+            maxZoomPreference = 20f,
+            minZoomPreference = 15f
+        ),
+        uiSettings = MapUiSettings(
+            zoomControlsEnabled = false
+
+        ),
+        cameraPositionState = cameraPositionState
     )
 }
 
