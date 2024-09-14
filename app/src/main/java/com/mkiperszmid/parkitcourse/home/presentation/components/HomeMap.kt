c6e06dcdb02faa6ce17729cc49ddc4e53b1869ed
Martin Kiperszmid <>
2024-09-14 15:46:17 -0300

    Improve map style

--- app/src/main/java/com/mkiperszmid/parkitcourse/home/presentation/components/HomeMap.kt
+++ app/src/main/java/com/mkiperszmid/parkitcourse/home/presentation/components/HomeMap.kt
@@ -1,13 +1,21 @@
 package com.mkiperszmid.parkitcourse.home.presentation.components
 
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
@@ -15,8 +23,10 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.mkiperszmid.parkitcourse.R
 import com.mkiperszmid.parkitcourse.home.domain.model.Location
 import com.mkiperszmid.parkitcourse.home.domain.model.Route
+import com.mkiperszmid.parkitcourse.ui.theme.AccentColor
 
 @Composable
 fun HomeMap(
@@ -26,6 +36,10 @@ fun HomeMap(
     modifier: Modifier = Modifier
 ) {
     val cameraPositionState = rememberCameraPositionState()
+    val context = LocalContext.current
+    val mapstyle = remember {
+        MapStyleOptions.loadRawResourceStyle(context, R.raw.mapstyle)
+    }
 
     LaunchedEffect(currentLocation) {
         currentLocation?.let {
@@ -45,11 +59,11 @@ fun HomeMap(
         properties = MapProperties(
             isMyLocationEnabled = true,
             maxZoomPreference = 20f,
-            minZoomPreference = 15f
+            minZoomPreference = 15f,
+            mapStyleOptions = mapstyle
         ),
         uiSettings = MapUiSettings(
             zoomControlsEnabled = false
-
         ),
         cameraPositionState = cameraPositionState
     ) {
@@ -58,19 +72,35 @@ fun HomeMap(
         }
 
         route?.let { route ->
-            Polyline(points = route.polylines.map { LatLng(it.latitude, it.longitude) })
+            Polyline(
+                points = route.polylines.map { LatLng(it.latitude, it.longitude) },
+                color = AccentColor
+            )
         }
     }
 }
 
 @Composable
 private fun CarMarker(position: Location) {
+    val context = LocalContext.current
     val state = remember {
         MarkerState(LatLng(position.latitude, position.longitude))
     }
-    Marker(state = state)
+    Marker(state = state, icon = bitmapDescriptorFromVector(context, R.drawable.ic_marker))
 }
 
+private fun bitmapDescriptorFromVector(context: Context, imageId: Int): BitmapDescriptor? {
+    val vectorDrawable = ContextCompat.getDrawable(context, imageId) ?: return null
+    vectorDrawable.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+    val bitmap = Bitmap.createBitmap(
+        vectorDrawable.intrinsicWidth,
+        vectorDrawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    vectorDrawable.draw(canvas)
+    return BitmapDescriptorFactory.fromBitmap(bitmap)
+}
 
 @Preview
 @Composable
