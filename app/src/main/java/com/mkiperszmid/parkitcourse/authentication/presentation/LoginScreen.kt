af798e9448449bbd5cb2a396edf8be6f6914ce50
Alberto Hidalgo <foliolo7891@gmail.com>
2024-09-28 01:16:54 +0200

    Clean imports

--- app/src/main/java/com/mkiperszmid/parkitcourse/authentication/presentation/LoginScreen.kt
+++ app/src/main/java/com/mkiperszmid/parkitcourse/authentication/presentation/LoginScreen.kt
@@ -3,15 +3,11 @@ package com.mkiperszmid.parkitcourse.authentication.presentation
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
